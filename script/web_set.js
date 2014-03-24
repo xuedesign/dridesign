2b9093837f92f581fd8c27c60828979c3377f5e4
xuedesign <elmo@xuedesign.tw>
2014-03-24 17:26:39 +0800

    增加retina圖片設定 #20140324

--- script/web_set.js
+++ script/web_set.js
@@ -29,7 +29,11 @@ $(function() {
  $("img").lazyload({placeholder : "../images/grey.gif",effect: "fadeIn"});
 });
 
-//
+// 載完網頁自動縮網址列
+window.addEventListener("load",function() {  
+	setTimeout(function(){
+	window.scrollTo(0, 1); }, 100);
+});
 
 	  
 
